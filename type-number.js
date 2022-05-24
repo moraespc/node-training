fadf3dc7e066950dbe1e01339b4fb6d2c75a8170
moraespc <pcmoraesf@gmail.com>
2022-05-24 00:03:00 -0300

    Exemplos de Tipos de Dados

---
+++ type-number.js
@@ -0,0 +1,17 @@
+
+// type Number
+const myNumber = 3;
+
+const num1 = 1;
+const num2 = 2;
+
+const opMat = num1 + num2;
+
+console.log(opMat);
+
+// ponto flutuante
+const pontoSemZero = .5;
+
+// NaN -> not a number
+const nome = "Jorginho";
+console.log(nome * pontoSemZero);
---
+++ type-string.js
@@ -0,0 +1,23 @@
+
+const text1 = "Meu texto";
+const text2 = 'Meu texto';
+
+const citacao = 'E Jonas disse: "Ele vira!"';
+console.log(citacao);
+
+//concatenacao (+)
+
+console.log(text1 + ' ' + citacao);
+
+//UTF codes
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+//template string OU template literal
