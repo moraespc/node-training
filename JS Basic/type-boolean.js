be7590edc7e50a67af8383230a6710d52a039ed5
moraespc <pcmoraesf@gmail.com>
2022-05-24 14:22:14 -0300

    Inicio de Node.js

--- JS Basic/type-boolean.js
+++ JS Basic/type-boolean.js
@@ -2,4 +2,10 @@
 const num1 = 5;
 const num2 = 10;
 
-console.log(num1 === num2);
+let isTrue = true;
+
+//console.log(num1 === num2);
+
+const text1 = "meu texto";
+const text2 = "testando";
+
---
+++ Node Basic/index.js
@@ -0,0 +1,8 @@
+
+// Importing module
+import chalk from 'chalk';
+  
+// Printing the text
+console.log(chalk.red("aayush"))
+console.log(chalk.underline("aayush"))
+console.log(chalk.underline.bold("GFG"))
